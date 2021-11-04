9ad6204b547d43c5bf9baa5aadd2f7de39838d60
Hyeong-Jun Kim <53989167+forceTlight@users.noreply.github.com>
2021-11-04 23:14:11 +0900

    Delete umc_yanolja directory

--- umc_yanolja/src/main/java/com/yanolja/repository/UserRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.yanolja.repository;
-
-import com.yanolja.domain.UserDTO;
-
-public interface UserRepositoryInterface {
-	public UserDTO insert(UserDTO user);
-	public Integer updateById(UserDTO user);
-	public Integer deleteById(Integer id);
-}
--- umc_yanolja/src/main/java/com/yanolja/service/UserServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.yanolja.service;
-
-import com.yanolja.domain.UserDTO;
-
-public interface UserServiceInterface {
-	public UserDTO insert(UserDTO user);
-	public Integer updateById(UserDTO user);
-	public Integer deleteById(Integer id);
-}
